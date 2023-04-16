3b67dfd14f94cecd2cba1a4435e493dd5ed137e9
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-04-16 16:19:05 -0300

    commit lesson 5

---
+++ Modulo_13_JavaScript_Intermediario/5 - Acessando elementos HTML com JS/script.js
@@ -0,0 +1,8 @@
+let p1 = document.getElementById("p1")
+let paragrafos = document.getElementsByClassName("paragrafo")
+let p = document.getElementsByTagName("p")
+
+for(let tp of paragrafos){
+    console.log(tp)
+}
+console.log(paragrafos)
