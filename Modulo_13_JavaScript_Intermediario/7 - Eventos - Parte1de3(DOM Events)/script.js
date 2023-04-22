3f371414ac29af430d7816eda582f5ec55049f6b
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-04-22 20:40:05 -0300

    update lesson 7

---
+++ Modulo_13_JavaScript_Intermediario/7 - Eventos - Parte1de3(DOM Events)/script.js
@@ -0,0 +1,14 @@
+// function h1click(){
+//     console.log("Executando a função")
+// }
+// function changeH1(input) {
+
+//     let h1 = document.getElementsByTagName("h1")[0]
+//     h1.innerText = input.value
+
+
+// } 
+function hideH1(){
+    let h1 = document.getElementsByTagName("h1")[0]
+    h1.style.display = "none";
+}
