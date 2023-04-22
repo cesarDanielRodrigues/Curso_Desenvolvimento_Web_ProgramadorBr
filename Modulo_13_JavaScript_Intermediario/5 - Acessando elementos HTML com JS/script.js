be728264065a37bc052f6e36b569824c74e3c0f6
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-04-22 18:53:19 -0300

    commit update script lesson 5

--- Modulo_13_JavaScript_Intermediario/5 - Acessando elementos HTML com JS/script.js
+++ Modulo_13_JavaScript_Intermediario/5 - Acessando elementos HTML com JS/script.js
@@ -1,8 +1,10 @@
 let p1 = document.getElementById("p1")
 let paragrafos = document.getElementsByClassName("paragrafo")
-let p = document.getElementsByTagName("p")
+let tp = document.getElementsByTagName("p")
+let th = document.getElementsByTagName("h1")
 
-for(let tp of paragrafos){
-    console.log(tp)
-}
-console.log(paragrafos)
+// for(let p of paragrafos){
+//     console.log(p)
+// }
+// console.log(paragrafos)
+console.log(th[0])
